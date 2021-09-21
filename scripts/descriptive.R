9970f4ee8fdb3d8c6017c49731cdcfae250c5586
Théophile THIERRY <theophile.thierry@insa-rouen.fr>
2021-09-21 19:18:05 +0200

    First scripts

---
+++ scripts/descriptive.R
@@ -0,0 +1,23 @@
+
+rm(list=ls())
+par(mfrow=c(1,1))
+
+data <- read.table("data/weatherAUS.csv", header=T, sep=",", na.string="NA", dec=".", quote="")
+data$Date <- as.Date(data$Date)
+
+statbase <- NULL
+numeric_names <- c()
+other_names <- c()
+for (i in names(data)) {
+  if (class(data[[i]]) %in% c("numeric", "integer")) {
+    statbase <- cbind(statbase, summary(data[[i]]))
+    numeric_names <- c(numeric_names, i)
+  }
+  else {
+    other_names <- c(other_names, i)
+  }
+}
+colnames(statbase) <- numeric_names
+
+print("Summary of numerical values : ")
+print(statbase)
