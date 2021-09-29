196a12a20d64ad1080b70fae7a2320ff245bd8ce
thewophile-beep <theophile.thierry@yahoo.com>
2021-09-29 18:33:10 +0200

    Experimenting with ggplot2

--- scripts/descriptive.R
+++ scripts/descriptive.R
@@ -1,8 +1,13 @@
-
 rm(list=ls())
+library(ggplot2)
+library(lubridate)
 
 data <- read.table("data/weatherAUS.csv", header=T, sep=",", na.string="NA", dec=".", quote="")
 data$Date <- as.Date(data$Date)
+data$Day <- yday(data$Date)
+data$Year=format(data$Date, format="%Y")
+
+str(data)
 
 statbase <- NULL
 numeric_names <- c()
@@ -20,3 +25,17 @@ colnames(statbase) <- numeric_names
 
 print("Summary of numerical values : ")
 print(statbase)
+
+# Cities observed
+unique(data$Location)
+
+# Example with Albury
+data_Albury <- data[data$Location=="Albury",]
+plot_Albury_Rainfall_Year <- ggplot(data_Albury, aes(x=Day, y=Rainfall, na.rm=T, color=Year)) +
+  geom_bar(stat = "identity") +
+  facet_wrap( ~ Year)
+plot_Albury_Rainfall_Year
+
+plot_Albury_Rainfall_Daily <- ggplot(data_Albury, aes(x=Day, y=MinTemp, group=Day, color=Day, na.rm=T)) +
+  geom_point()  
+plot_Albury_Rainfall_Daily
